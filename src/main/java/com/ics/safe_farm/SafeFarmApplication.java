af91af11ed21542497e212cabc355dd07ce12471
gunwoong <gwj0421@gmail.com>
2023-11-22 17:46:38 +0900

    Initial commit

---
+++ src/main/java/com/ics/safe_farm/SafeFarmApplication.java
@@ -0,0 +1,13 @@
+package com.ics.safe_farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeFarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeFarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ics/safe_farm/SafeFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ics.safe_farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeFarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
